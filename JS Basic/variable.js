13e2d80631e00557c7df607599b2289d770f53b5
Dullguunuu <dulguun.nst@gmail.com>
2022-11-28 12:21:25 +0800

    added

---
+++ JS Basic/variable.js
@@ -0,0 +1,93 @@
+// Daalgavar 1
+
+// Comments can make code readable
+
+// Daalgavar 2
+
+// Javascript lesson begin
+
+// Daalgavar 3
+
+/* Comments can make code readable
+Comments can make code readable
+Comments can make code readable */
+
+// Daalgavar 4
+
+let a = 2;
+console.log(a)
+
+let b = "This is string";
+console.log(b)
+
+let c = true;
+console.log(c)
+
+let d;
+console.log(d)
+
+let e = null;
+console.log(e)
+
+// Daalgavar 5 
+
+let a1 = 3, b1 = "string", c1 = false, d1, e1 = null
+console.log(typeof (a1), (b1), (c1), (d1), (e1))
+
+// Daalgavar 6
+
+let f;
+
+// Daalgavar 7 
+
+let g = 1;
+console.log(g)
+
+// Daalgavar 8
+
+var firstName = "Dulguun";
+console.log(firstName)
+var lastName = "Bayanmunkh";
+console.log(lastName)
+var maritalStatus = "Married";
+console.log(maritalStatus)
+var country = "Mongolia";
+console.log(country)
+var age = 25;
+console.log(age)
+
+// Daalgavar 9
+
+var first_name = "Dulguun", last_name = "Bayanmunkh", marrital_status = "Married", country_a = "Mongolia", age_b = 25;
+console.log((first_name), (last_name), (marrital_status), (country_a), (age_b))
+
+// Daalgavar 10
+
+let myAge = 21, yourAge = 22;
+console.log(myAge, yourAge)
+
+// Daalgavar 11
+
+let p1 = 10, p2 = 10, p3 = 10;
+console.log(p = p1 + p2 + p3)
+
+// Daalgavar 12
+
+let r = 5, pi = 3.14;
+console.log("s=", pi * r ** 2)
+
+
+// Daalgavar 13
+
+let y2 = 25, y1 = 5, x2 = 19, x1 = 12
+console.log("m=", (y2 - y1) / (x2 - x1))
+
+// Daalgavar 16
+
+let bit = 8, byte = 8 * bit, KB = 1024 * byte, MB = 1024 * KB, GB = 1024 * MB
+console.log("15 Gegabyte =", 15 * GB, "bits")
+
+// Daalgavar 17
+
+// let
+//     console.log("BIT Rate" =)
