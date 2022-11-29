62cd8cce0cbd9180db1663a73fede1537f135972
Dullguunuu <dulguun.nst@gmail.com>
2022-11-29 19:03:11 +0800

    added

--- JS Basic/variable.js
+++ JS Basic/variable.js
@@ -58,36 +58,133 @@ console.log(age)
 
 // Daalgavar 9
 
-var first_name = "Dulguun", last_name = "Bayanmunkh", marrital_status = "Married", country_a = "Mongolia", age_b = 25;
+var first_name = "Dulguun", last_name = "Bayanmunkh", marrital_status = "djsdcnkl", country_a = "Mongolia", age_b = 25;
 console.log((first_name), (last_name), (marrital_status), (country_a), (age_b))
 
 // Daalgavar 10
 
-let myAge = 21, yourAge = 22;
-console.log(myAge, yourAge)
-
-// Daalgavar 11
-
 let p1 = 10, p2 = 10, p3 = 10;
 console.log(p = p1 + p2 + p3)
 
-// Daalgavar 12
+// Daalgavar 11
 
 let r = 5, pi = 3.14;
 console.log("s=", pi * r ** 2)
 
+// Daalgavar 12
 
-// Daalgavar 13
-
-let y2 = 25, y1 = 5, x2 = 19, x1 = 12
+let y2 = 25, y1 = 5, x2 = 19, x1 = 12;
 console.log("m=", (y2 - y1) / (x2 - x1))
 
-// Daalgavar 16
+// Daalgavar 13
 
 let bit = 8, byte = 8 * bit, KB = 1024 * byte, MB = 1024 * KB, GB = 1024 * MB
 console.log("15 Gegabyte =", 15 * GB, "bits")
 
+// Daalgavar 14
+ 
+let Mb = 15, s = 3;
+console.log("BIT Rate =", Mb / s , "Mbps")
+
+// Daalgavar 15
+
+let side1 = 20, side2 = 30, side3 = 40
+
+
+
+
+// Daalgavar 16
+
+let C = 30;
+console.log("F =", (C * 9/5)+32 )
+
 // Daalgavar 17
 
-// let
-//     console.log("BIT Rate" =)
+let F = 105;
+console.log("C =", (F -32)* 5/9 )
+
+// Daalgavar 18
+
+let k = 15, q = 10;
+console.log("+ =", k+q)
+console.log("- =", k-q)
+console.log("* =", k*q)
+console.log("/ =", k/q)
+console.log("% =", k%q)
+
+// Daalgavar 19
+
+let cm = 15, inch = 20;
+console.log("inch =", 2.54*cm)
+console.log("cm =", 0.3937*inch)
+
+// Daalgavar 20
+
+let radius=5, piNumber=3.14;
+console.log("toirgiin ezelhuun =", 4/3*piNumber*radius*radius*radius)
+console.log("toirgiin urt =", 2*piNumber*radius)
+
+// Daalgavar 21
+
+let abc=976;
+console.log("taslalaas umnuh too ni 3 orontoi toonii ehnii tsifr shuu bagshaa kk ", abc/100)
+
+// Daalgavar 22
+
+let v1= "BaBy Shark";
+let v2= "doo-doo, doo-doo";
+let v3= "Mommy Shark";
+let v4= "Daddy Shark";
+let v5= "Grandma Shark";
+let v6= "Grandpa Shark";
+let v7= "Let's go hunt";
+let v8= "Run away";
+let v9= "(ah!)";
+let v10= "Safe at last";
+let v11= "(phew)";
+let v12= "It's the end";
+
+console.log(v1, "," , v2)
+console.log(v1, "," , v2)
+console.log(v1, "," , v2)
+console.log(v1)
+
+console.log(v3, "," , v2)
+console.log(v3, "," , v2)
+console.log(v3, "," , v2)
+console.log(v3)
+
+console.log(v4, "," , v2)
+console.log(v4, "," , v2)
+console.log(v4, "," , v2)
+console.log(v4)
+
+console.log(v5, "," , v2)
+console.log(v5, "," , v2)
+console.log(v5, "," , v2)
+console.log(v5)
+
+console.log(v6, "," , v2)
+console.log(v6, "," , v2)
+console.log(v6, "," , v2)
+console.log(v6)
+
+console.log(v7, "," , v2)
+console.log(v7, "," , v2)
+console.log(v7, "," , v2)
+console.log(v7)
+
+console.log(v8, "," , v2)
+console.log(v8, "," , v2)
+console.log(v8, "," , v2)
+console.log(v8, v9)
+
+console.log(v10, "," , v2)
+console.log(v10, "," , v2)
+console.log(v10, "," , v2)
+console.log(v10, v11)
+
+console.log(v12, "," , v2)
+console.log(v12, "," , v2)
+console.log(v12, "," , v2)
+console.log(v12)
