125ddb50a07314fcfe63aa64aac46ce0a28f5328
Dullguunuu <dulguun.nst@gmail.com>
2022-11-30 11:13:15 +0800

    added

---
+++ JS Condition/Хувьсагч/huvisagch.js
@@ -0,0 +1,28 @@
+// 1
+
+let a = 85, b = 75, c = 96, d = 69;
+
+if (a>80)
+{
+    console.log(a)
+}
+
+else if (b>80)
+{
+    console.log(a++b)
+}
+
+else if (c>80)
+{
+    console.log(a++c)
+}
+
+else (d>80)
+{
+    console.log(i++d)
+}
+
+// 2
+
+// let x = 3, y = 7, z = 2, w = 4;
+// if ()
---
+++ JS Condition/хамгийн их болон багыг олно уу/main.js
@@ -0,0 +1,31 @@
+let a = 2344, b = 534, c = 45;
+if (a>b && a>c)
+{
+    console.log("Хамгийн их тоо нь", a)
+}
+
+else if  (b>a && b>c)
+{
+    console.log("Хамгийн их тоо нь", b)
+}
+
+else
+{
+    console.log("Хамгийн их тоо нь", c)
+}
+
+if (a<b && a<c)
+{
+    console.log("Хамгийн бага тоо нь", a)
+}
+
+else if  (b<a && b<c)
+{
+    console.log("Хамгийн бага тоо нь", b)
+}
+
+else
+{
+    console.log("Хамгийн бага тоо нь", c)
+}
+
