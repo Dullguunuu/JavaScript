d057f2568d3c03c2277dc77e3cbc257d54176de0
Dullguunuu <dulguun.nst@gmail.com>
2022-12-05 12:43:51 +0800

    added

---
+++ Array/SimpleArray-1.js
@@ -0,0 +1,17 @@
+const itCompanies = ["Facebook", "Google", "Microsoft", "Apple", "IBM", "Oracle", "Amazon"];
+
+// 
+
+console.log(itCompanies);
+
+// 
+
+for (i = 0; i < itCompanies.length; i++) {
+    console.log(itCompanies[i] + "-n urt " + itCompanies[i].length)
+}
+
+// 
+
+console.log(itCompanies[0], itCompanies[itCompanies.length[4]] , itCompanies.[6]);
+
+// 
--- JS Number/Register Number Validation/registerNumber.js
+++ JS Number/Register Number Validation/registerNumber.js
@@ -1,24 +1,13 @@
-let number = prompt ("Та регистрийн дугаараа оруулна уу");
+let number = prompt("Та регистрийн дугаараа оруулна уу");
 
-if (number.length == 10)
-{
-    if (number.substring(0,2) != Number(number.substring(0,2)))
-    {
-        number.substring(2, 10) == Number(number.substring(2, 10)) ? console.log("Та монгол улсын регистрийн дугаарыг зөв орууллаа") : console.log("Регистрийн сүүлийн 8 орон тоо байхыг анхаарна уу");
+if (number.length == 10) {
+    if (number.substring(0, 1) != Number(number.substring(0, 1))) {
+        number.substring(2, 9) == Number(number.substring(2, 9)) ? console.log("Та монгол улсын регистрийн дугаарыг зөв орууллаа") : console.log("Регистрийн сүүлийн 8 орон тоо байхыг анхаарна уу");
     }
-    else 
-        {
-            console.log("Регистрийн эхний 2 орон үсэг байхыг анхаарна уу");
-        }
+    else {
+        console.log("Регистрийн эхний 2 орон үсэг байхыг анхаарна уу");
+    }
 }
-else
-{
+else {
     console.log("Та регистрийн дугаараа зөв оруулсан эсэхээ шалгана уу");
 }
-
-
-// number.length == 10 ?     : console.log("Та регистрийн дугаараа зөв оруулсан эсэхээ шалгана уу");
-// number.substring(0,2) != Number(number.substring(0,2)) ?           : console.log("Регистрийн эхний 2 орон үсэг байхыг анхаарна уу");
-// number.substring(2, 10) == Number(number.substring(2, 10)) ? console.log("Та монгол улсын регистрийн дугаарыг зөв орууллаа") : console.log("Регистрийн сүүлийн 8 орон тоо байхыг анхаарна уу");
-
-
