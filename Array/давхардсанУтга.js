3ecd48fb83663c7be9c1ced4f1a5a55f21680a0c
Dullguunuu <dulguun.nst@gmail.com>
2022-12-06 12:39:50 +0800

    added

--- Array/давхардсанУтга.js
+++ Array/давхардсанУтга.js
@@ -1,9 +1,23 @@
-let array = [4, 2, 34, 4, 1, 12, 1, 4];
+let array = [4, 2, 34, 4, 1, 1, 12, 1, 4];
+let output = [];
+let ii = 0;
 
-for (let i = 0; i < array.length; i++)
-{
-    if (Number(array.indexOf(toString(array[i]))) > 0)
-    {
-        console.log("давхардсан тоо байна")
+
+//0 -> 4 
+//1
+
+for (let i = 0; i < array.length; i++) {
+
+    let b = array[i];
+    for (let j = i + 1; j < array.length; j++) {
+        if (b === array[j]) {
+            output.length++
+        }
     }
 }
+
+console.log(output)
+
+//index = 0
+
+//length = 1
