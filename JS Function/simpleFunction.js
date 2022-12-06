77fc053f17f2c9dba6fa51e2a74802d60d446d35
Dullguunuu <dulguun.nst@gmail.com>
2022-12-06 23:57:05 +0800

    Dec06

---
+++ JS Function/simpleFunction.js
@@ -0,0 +1,42 @@
+// 1
+
+function fullName() {
+    console.log("Dulguun Bayanmunkh")
+}
+
+fullName();
+
+// 2
+
+function fName(firstName, lastName) {
+    return "Hello" + ' ' + firstName + ' ' + lastName;
+}
+
+console.log(fName("Bilguun", "Bayanmunkh"))
+
+// 3
+
+function sum(a, b) {
+    console.log(a + b);
+}
+
+sum(2, 3);
+sum(123, 45678)
+
+// 4
+
+function area(x, y)
+{
+    console.log(x*y)
+}
+
+area(4,5)
+
+// 5
+
+function perimeter(u,v)
+{
+    console.log(2*(u+v));
+}
+
+perimeter(6, 12)
---
+++ JS Function/функцуудЗохиох.js
@@ -0,0 +1,18 @@
+// 1
+
+function max (a ,b)
+{
+    console.log(Math.max(a,b))
+}
+max (2,3)
+
+// 2
+
+function array (a1, a2, a3, a4, a5) {
+    function n (n){
+
+    console.log()
+    }
+}
+
+array = [3, 45, 23, 78, 34];
