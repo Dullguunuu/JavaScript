e80277fe8321382b7fc9cdb3fa85cde5cbac9b8b
Dullguunuu <dulguun.nst@gmail.com>
2022-12-20 12:45:52 +0800

    dec20

---
+++ OnooniiSambar/action.js
@@ -0,0 +1,21 @@
+document.getElementById("title1").innerHTML = "Дүн оруулах"
+
+let firstName = document.getElementById("fName");
+let lastName = document.getElementById("lName");
+let option1 = document.getElementById("opt1");
+let option2 = document.getElementById("opt2");
+let dun = document.getElementById("score");
+
+
+firstName.placeholder = "Сурагчийн нэр"
+lastName.placeholder = "Овог"
+dun.placeholder = "Оноо"
+
+// Jagsaalt garah heseg 
+
+document.getElementById("title2").innerHTML = "Дүнгийн жагсаалт"
+
+
+{/* <i class="fa fa-pencil" aria-hidden="true"></i>
+            <i class="fa fa-check" aria-hidden="true"></i>
+            <i class="fa fa-trash" aria-hidden="true"></i> */}
