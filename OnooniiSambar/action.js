a3b33a3fe7fc8d1c461b7a4a80bca8121eada7bd
Dullguunuu <dulguun.nst@gmail.com>
2022-12-22 12:22:20 +0800

    dec22

--- OnooniiSambar/action.js
+++ OnooniiSambar/action.js
@@ -1,21 +1,55 @@
 document.getElementById("title1").innerHTML = "Дүн оруулах"
 
-let firstName = document.getElementById("fName");
-let lastName = document.getElementById("lName");
-let option1 = document.getElementById("opt1");
-let option2 = document.getElementById("opt2");
-let dun = document.getElementById("score");
+let fName = document.getElementById("fName");
+let lName = document.getElementById("lName");
+let opt1 = document.getElementById("opt1");
+let opt2 = document.getElementById("opt2");
+let score = document.getElementById("score");
+let dun_oroh = document.getElementById("dun_oroh");
 
-
-firstName.placeholder = "Сурагчийн нэр"
-lastName.placeholder = "Овог"
-dun.placeholder = "Оноо"
+fName.placeholder = "Сурагчийн нэр"
+lName.placeholder = "Овог"
+score.placeholder = "Оноо"
 
 // Jagsaalt garah heseg 
 
 document.getElementById("title2").innerHTML = "Дүнгийн жагсаалт"
 
+let dun = [];
 
-{/* <i class="fa fa-pencil" aria-hidden="true"></i>
-            <i class="fa fa-check" aria-hidden="true"></i>
-            <i class="fa fa-trash" aria-hidden="true"></i> */}
+function array_nemeh(event) {
+
+    let gender = ""
+    if (opt1.checked) { gender = "эмэгтэй" } else { gender = "эрэгтэй" };
+    let obj = {
+        firstName: fName.value, lastName: lName.value, huis: gender, avsanDun: score.value
+    }
+    dun.push(obj);
+    input_nemeh();
+    event.preventDefault();
+}
+
+function input_nemeh() {
+    let list = "";
+    for (let i = 0; i < dun.length; i++) {
+        list += `<div class = "murStyle">
+    <p class = "fNameEl">${dun[i].firstName}</p>
+    <p class = "lNameEl">${dun[i].lastName}</p>
+    <p class = "genEl">${dun[i].huis}</p>
+    <p class = "scoreEl">${dun[i].avsanDun}</p>
+    <div>
+    <i class="fa fa-pencil onclick = "editScore()" aria-hidden="true"></i>
+    <i class="fa fa-trash onclick = "delEl()" aria-hidden="true"></i>
+    </div>
+    </div>`
+    }
+    dun_oroh.innerHTML = list;
+}
+
+function editScore() {
+
+}
+
+function delEl() {
+
+}
