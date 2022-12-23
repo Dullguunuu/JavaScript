9bb6fe24af3e566c4f86e04d640277226178cca9
Dullguunuu <dulguun.nst@gmail.com>
2022-12-23 09:20:51 +0800

    dec23

--- JS Dom/Stopwatch/stopWatch.js
+++ JS Dom/Stopwatch/stopWatch.js
@@ -1,65 +1,17 @@
+let watch = document.getElementById("watch")
+let startBtn = document.getElementById("startBtn")
+let stopBtn = document.getElementById("stopBtn")
 
+let timer = new Date();
+timer.setMinutes(0, 0, 0);
+watch.innerHTML = `${timer.getMinutes()} - ${timer.getSeconds()} - ${timer.getMilliseconds()}`;
 
-// time.setMinutes(0, 0, 0)
+// startBtn.addEventListener("click", ()=>{
+//     let timer = setTimeout(,1000)
+// })
 
-// let min = time.getMinutes();
-// let sec = time.getSeconds();
-// let mlSec = time.getMilliseconds();
-// console.log(min + " : " + sec + " : " + mlSec)
+// stopBtn.addEventListener("click", ()=>{
 
-// let tsag = document.getElementById("main")
-// tsag.appendChild(date)
+// })
 
-// let timeoutID;
-// function showAlert() {
-// timeoutID = setTimeout(alert, 3000, 'setTimeout Demo!');
-// }
-// function clearAlert() {
-// clearTimeout(timeoutID);
-// }
-
-// let counte = 0;
-// setInterval(startInterval, 2000)
-// function startInterval(){
-// console.log(counte)
-// counte++;
-// }
-
-
-let mainDiv = document.querySelector(".main");
-let timer = document.querySelector(".timeArea");
-let remain = document.querySelector(".rem");
-let btn = document.getElementsByTagName("button");
-
-let timing = new Date();
-
-let day = `${timing.getDate()}`;
-let hour = `${timing.getHours()}`;
-let min = `${timing.getMinutes()}`;
-let sec = `${timing.getSeconds()}`;
-
-let dayRem = document.createElement("span");
-let hourRem = document.createElement("span");
-let minRem = document.createElement("span");
-let secRem = document.createElement("span");
-
-dayRem.innerHTML = 
-hourRem.innerHTML = 
-minRem.innerHTML = 
-secRem.innerHTML = 
-
-day.appendChild(dayRem)
-hour.appendChild(hourRem)
-min.appendChild(minRem)
-sec.appendChild(secRem)
-
-console.log(day)
-
-
-
-
-
-
-
-
-
+// watch.innerHTML = `${date.getMinutes()} - ${date.getSeconds()} - ${date.getMilliseconds()}`
--- Projects/OnooniiSambar/action.js
+++ Projects/OnooniiSambar/action.js
@@ -5,6 +5,7 @@ let lName = document.getElementById("lName");
 let opt1 = document.getElementById("opt1");
 let opt2 = document.getElementById("opt2");
 let score = document.getElementById("score");
+let second = document.getElementById("second")
 let dun_oroh = document.getElementById("dun_oroh");
 
 fName.placeholder = "Сурагчийн нэр"
@@ -18,15 +19,18 @@ document.getElementById("title2").innerHTML = "Дүнгийн жагсаалт"
 let dun = [];
 
 function array_nemeh(event) {
-
-    let gender = ""
-    if (opt1.checked) { gender = "эмэгтэй" } else { gender = "эрэгтэй" };
+    let gender = "";
+    if (opt1.checked) { gender = "эмэгтэй" }
+    else { gender = "эрэгтэй" };
     let obj = {
         firstName: fName.value, lastName: lName.value, huis: gender, avsanDun: score.value
     }
     dun.push(obj);
     input_nemeh();
     event.preventDefault();
+    fName.value = "";
+    lName.value = "";
+    score.value = "";
 }
 
 function input_nemeh() {
@@ -38,18 +42,20 @@ function input_nemeh() {
     <p class = "genEl">${dun[i].huis}</p>
     <p class = "scoreEl">${dun[i].avsanDun}</p>
     <div>
-    <i class="fa fa-pencil onclick = "editScore()" aria-hidden="true"></i>
-    <i class="fa fa-trash onclick = "delEl()" aria-hidden="true"></i>
+    <button type = "button" onclick = "editBtn(${i})" id = "editBtn">Дүн нэмэх</button>
+    <button type = "button" onclick = "delBtn(${i})" id = "delBtn">Устгах</button>
     </div>
     </div>`
     }
     dun_oroh.innerHTML = list;
 }
 
-function editScore() {
-
+function editBtn(id) {
+    dun[id].avsanDun = Number(dun[id].avsanDun) + 5;
+    input_nemeh();
 }
 
-function delEl() {
-
+function delBtn(id) {
+    dun.splice(id, 1);
+    input_nemeh();
 }
